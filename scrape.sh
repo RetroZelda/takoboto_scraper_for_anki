4cbbd04f431bad9e00ef12d2aa19741650c21138
RetroZelda <efolckemer@retrozelda.com>
2023-05-21 23:20:33 -0400

    updated scrape script to start anki if its not running and to clean up after itsself

--- scrape.sh
+++ scrape.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#!/bin/bash
 
 # Create the virtual environment
 venv_name="japanese_builder"
@@ -12,7 +13,36 @@ mkdir .tmp
 
 # scrape and upload to Anki(NOTE: Requires Anki Connect to be running)
 python3 ./python/scrape_takoboto.py ./data/verbs_to_scrape.csv ./.tmp/verbs.json
-python3 ./python/generate_anki.py ./.tmp/verbs.json
+
+if [ -f "./.tmp/verbs.json" ]; then
+
+    # Check if the anki is running
+    program_name="anki"
+    if pgrep -x "$program_name" >/dev/null; then
+        echo "$program_name is already running."
+    else
+        # Start the program
+        echo "Starting $program_name..."
+        anki >/dev/null 2>&1 &
+
+        # Wait for 1 seconds for it to start
+        sleep 1
+    fi
+
+    # Check if the program started successfully
+    if pgrep -x "$program_name" >/dev/null; then
+        echo "$program_name connected successfully."
+
+        python3 ./python/generate_anki.py ./.tmp/verbs.json
+        rm ./.tmp/verbs.json
+    else
+        echo "Failed to start $program_name."
+    fi
+else
+    echo "No new data to import."
+fi
+
+rm -d .tmp
 
 # Deactivate the virtual environment
 deactivate
